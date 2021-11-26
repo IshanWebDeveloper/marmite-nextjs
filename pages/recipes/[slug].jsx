90b7f90ba56f401c0749f8e2f05237e9ae80d99a
IshanWebDeveloper <ishananaguru@gmail.com>
2021-11-27 04:44:00 +0530

    fallback

--- pages/recipes/[slug].jsx
+++ pages/recipes/[slug].jsx
@@ -1,6 +1,7 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { createClient } from 'contentful';
 import Image from 'next/image';
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -8,6 +9,12 @@ const client = createClient({
 });
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe)
+    return (
+      <div>
+        <Skeleton />
+      </div>
+    );
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
   return (
@@ -87,6 +94,6 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
